00e1e031373b02de841b74c418f9bf1d6e5c7021
Daniel Vianna <dmlvianna@gmail.com>
2018-01-19 16:37:45 +1100

    last one before graphing

---
+++ src/03/Main.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
+module Main where
+
+import           Data.Csv             (FromNamedRecord (..), (.:))
+import           Data.Foldable        (toList)
+import           Data.Text            (Text)
+import           Data.Vector          (Vector)
+
+import qualified Data.ByteString.Lazy
+import qualified Data.Csv
+import qualified Data.List
+
+data Specimen = Specimen
+  { institutionCode :: Text
+  } deriving (Show)
+
+instance FromNamedRecord Specimen where
+  parseNamedRecord m = do
+    institutionCode <- m .: "dwc:institutionCode"
+    return (Specimen {..})
+
+process :: FilePath -> IO (Vector Specimen)
+process file = do
+  bytes <- Data.ByteString.Lazy.readFile file
+  case Data.Csv.decodeByName bytes of
+    Left err          -> fail err
+    Right (_, vector) -> return vector
+
+main :: IO ()
+main = do
+  specimens <- process "./data/holothuriidae-specimens.csv"
+  let institutions :: Vector Text
+      institutions = fmap institutionCode specimens
+  print (Data.List.nub (toList institutions))
+  -- ["FLMNH","CAS","MCZ","YPM"]
